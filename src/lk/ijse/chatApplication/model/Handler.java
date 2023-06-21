baccf89bd8bf402d64ba62c54ea2999889d44761
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-06-21 17:19:47 +0530

    Server Implemented

--- src/lk/ijse/chatApplication/model/Handler.java
+++ src/lk/ijse/chatApplication/model/Handler.java
@@ -1,9 +1,30 @@
 package lk.ijse.chatApplication.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * @author : Kavithma Thushal
  * @since : 6/21/2023
  **/
-public class Handler {
+public class Handler extends Thread {
+    private ArrayList<Handler> clients;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
+    public Handler(Socket socket, ArrayList<Handler> clients) {
+        try {
+            this.socket = socket;
+            this.clients = clients;
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/lk/ijse/chatApplication/model/Server.java
+++ src/lk/ijse/chatApplication/model/Server.java
@@ -1,5 +1,8 @@
 package lk.ijse.chatApplication.model;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 /**
@@ -7,5 +10,24 @@ import java.util.ArrayList;
  * @since : 6/21/2023
  **/
 public class Server {
-    private ArrayList<Handler> clients = new ArrayList<>();
+    private static ArrayList<Handler> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket=new ServerSocket(1)){
+            Socket socket;
+
+            while (true){
+                System.out.println("Waiting for client ...");
+                socket = serverSocket.accept();
+                System.out.println("Client is connected");
+
+                Handler handler = new Handler(socket, clients);
+                clients.add(handler);
+                handler.start();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
